5f23e92cf613457ae2143841afb0933ceb2395de
dalabi91 <152018805+dalabi91@users.noreply.github.com>
2024-02-28 16:48:44 +0000

    some code refatoring

--- index.js
+++ index.js
@@ -13,21 +13,13 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
-//class containing all questions
-class Prompt {
-  constructor() {
-    this.teamInfo = []; //initialse empty array to store team members info
-  }
-  //method to get all employee array of all object
-  getTeamInfo() {
-    return this.teamInfo;
-  }
-  //array of question for members
+this.teamInfo = []; //initialize empty array to store team members info
 
-  question() {
-    inquirer
-      .prompt({
-        //employer type
+function promptUserType() {
+  inquirer
+    .prompt([
+      //employer type
+      {
         type: "list",
         name: "employerType",
         message: "Select employer type to add to team?",
@@ -37,264 +29,277 @@ class Prompt {
           "Intern",
           "I finished entering my team info",
         ],
-      })
-      .then(({ employeeType }) => {
-        // prompt for specific employee details based on the user's choice,
-        if (employeeType === "Manager") {
-          inquirer
-            .prompt([
-              // questions for Manager details
-              //Name
-              {
-                type: "List",
-                name: "name",
-                message: "Please enter manager's name",
-                validate: (nameInput) => {
-                  if (nameInput) {
-                    return true;
-                  } else {
-                    console.log("Please enter manager's name!");
-                    return false;
-                  }
-                },
-              },
-
-              //Employee ID
-              {
-                type: "number",
-                name: "id",
-                message: "Please enter manager's id",
-                validate: (idInput) => {
-                  if (idInput) {
-                    return true;
-                  } else {
-                    console.log(
-                      "Please enter the correct manager's id, this should be a number!"
-                    );
-                    return false;
-                  }
-                },
-              },
-
-              //Email address
-              {
-                type: "input",
-                name: "email",
-                message: "Please enter manager's email",
-                validate: (emailInput) => {
-                  if (emailInput) {
-                    return true;
-                  } else {
-                    console.log("Please enter the correct manager's email!");
-                    return false;
-                  }
-                },
-              },
-
-              //Office number
-              {
-                type: "number",
-                name: "officeNumber",
-                message: "Please enter manager's office Number",
-                validate: (idInput) => {
-                  if (idInput) {
-                    return true;
-                  } else {
-                    console.log(
-                      "Please enter the correct manager's office Number, this should be a number!"
-                    );
-                    return false;
-                  }
-                },
-              },
-            ])
-            .then((managerData) => {
-              // Create a new Manager object and push it to teamInfo Array
-              const newManager = new Manager(
-                managerData.name,
-                managerData.id,
-                managerData.email,
-                managerData.officeNumber
-              );
-              this.teamInfo.push(newManager);
-              // Send user back to the menu for more input
-              this.question();
-            });
-
-          // Similar prompts and actions for Engineer and Intern
-        } else if (employeeType === "Engineer") {
-          inquirer
-            .prompt([
-              //Name
-              {
-                type: "input",
-                name: "name",
-                message: "Please enter engineer's name",
-                validate: (nameInput) => {
-                  if (nameInput) {
-                    return true;
-                  } else {
-                    console.log("Please enter the correct engineer's name!");
-                    return false;
-                  }
-                },
-              },
-
-              {
-                type: "number",
-                name: "id",
-                message: "Please enter engineer's id",
-                validate: (idInput) => {
-                  if (idInput) {
-                    return true;
-                  } else {
-                    console.log(
-                      "Please enter the correct engineer's id, this should be a number!"
-                    );
-                    return false;
-                  }
-                },
-              },
-
-              //Email address
-              {
-                type: "input",
-                name: "email",
-                message: "Please enter engineer's email",
-                validate: (emailInput) => {
-                  if (emailInput) {
-                    return true;
-                  } else {
-                    console.log("Please enter the correct engineer's email!");
-                    return false;
-                  }
-                },
-              },
-
-              //Office number
-              {
-                type: "number",
-                name: "officeNumber",
-                message: "Please enter engineer's office Number",
-                validate: (idInput) => {
-                  if (idInput) {
-                    return true;
-                  } else {
-                    console.log(
-                      "Please enter the correct engineer's office Number, this should be a number!"
-                    );
-                    return false;
-                  }
-                },
-              },
-            ])
-            //pushes engineer;s data into teamInfo array
-            .then((engineerData) => {
-              // Create a new Manager object and push it to teamInfo Array
-              const newEngineer = new Engineer(
-                engineerData.name,
-                engineerData.id,
-                engineerData.email,
-                engineerData.officeNumber
-              );
-              this.teamInfo.push(newEngineer);
-              // Send user back to the menu for more input
-              this.question();
-            });
-        } else if (employeeType === "Intern") {
-          inquirer
-            .prompt([
-              //Name
-              {
-                type: "input",
-                name: "name",
-                message: "Please enter Intern's name",
-                validate: (nameInput) => {
-                  if (nameInput) {
-                    return true;
-                  } else {
-                    console.log("Please enter the correct Intern's name!");
-                    return false;
-                  }
-                },
-              },
-
-              {
-                type: "number",
-                name: "id",
-                message: "Please enter Intern's id",
-                validate: (idInput) => {
-                  if (idInput) {
-                    return true;
-                  } else {
-                    console.log(
-                      "Please enter the correct Intern's id, this should be a number!"
-                    );
-                    return false;
-                  }
-                },
-              },
-
-              //Email address
-              {
-                type: "input",
-                name: "email",
-                message: "Please enter Intern's email",
-                validate: (emailInput) => {
-                  if (emailInput) {
-                    return true;
-                  } else {
-                    console.log("Please enter the correct Intern's email!");
-                    return false;
-                  }
-                },
-              },
-
-              //Office number
-              {
-                type: "number",
-                name: "officeNumber",
-                message: "Please enter Intern's office Number",
-                validate: (idInput) => {
-                  if (idInput) {
-                    return true;
-                  } else {
-                    console.log(
-                      "Please enter the correct Intern's office Number, this should be a number!"
-                    );
-                    return false;
-                  }
-                },
-              },
-            ])
-            //pushes intern's data into teamInfo array
-            .then((internData) => {
-              // Create a new Manager object and push it to teamInfo Array
-              const newIntern = new Intern(
-                internData.name,
-                internData.id,
-                internData.email,
-                internData.officeNumber
-              );
-              this.teamInfo.push(newIntern);
-              // Send user back to the menu for more input
-              this.question();
-            });
-        } else if (employeeType === "I finished entering my team info") {
-          // Generate HTML page based on teamArray and write it to a file
-          const htmlPage = render(this.getTeamInfo());
-          fs.writeFile(outputPath, htmlPage, (err) =>
-            err
-              ? console.log(err)
-              : console.log("HTML file has been created in the lib/folder")
-          );
-        }
-      });
-  }
+      },
+    ])
+    .then((response) => {
+      // call the correct function based on user choice
+      switch (response.menu) {
+        case "Add an engineer":
+          engineerPrompt();
+          break;
+        case "Add an intern":
+          internPrompt();
+          break;
+        case "I finished entering my team info":
+          // Generate the HTML and finish the process
+          generateHTML();
+          break;
+      }
+    });
 }
 
-// Activates prompts in
-const prompt = new Prompt();
-prompt.question();
+// questions for Manager details
+function managerPrompt() {
+  inquirer
+    .prompt([
+      //Name
+      {
+        type: "List",
+        name: "name",
+        message: "Please enter manager's name",
+        validate: (nameInput) => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log("Please enter manager's name!");
+            return false;
+          }
+        },
+      },
 
-// module.exports = Prompt;
+      //Employee ID
+      {
+        type: "number",
+        name: "id",
+        message: "Please enter manager's id",
+        validate: (idInput) => {
+          if (idInput) {
+            return true;
+          } else {
+            console.log(
+              "Please enter the correct manager's id, this should be a number!"
+            );
+            return false;
+          }
+        },
+      },
+
+      //Email address
+      {
+        type: "input",
+        name: "email",
+        message: "Please enter manager's email",
+        validate: (emailInput) => {
+          if (emailInput) {
+            return true;
+          } else {
+            console.log("Please enter the correct manager's email!");
+            return false;
+          }
+        },
+      },
+
+      //Office number
+      {
+        type: "number",
+        name: "officeNumber",
+        message: "Please enter manager's office Number",
+        validate: (idInput) => {
+          if (idInput) {
+            return true;
+          } else {
+            console.log(
+              "Please enter the correct manager's office Number, this should be a number!"
+            );
+            return false;
+          }
+        },
+      },
+    ])
+    .then((managerData) => {
+      // Create a new Manager object and push it to teamInfo Array
+      const newManager = new Manager(
+        managerData.name,
+        managerData.id,
+        managerData.email,
+        managerData.officeNumber
+      );
+      this.teamInfo.push(newManager);
+      // Send user back to the menu for more input
+      this.promptUserType();
+    });
+}
+
+// Similar prompts and actions for Engineer and Intern
+function engineerPrompt() {
+  inquirer
+    .prompt([
+      //Name
+      {
+        type: "input",
+        name: "name",
+        message: "Please enter engineer's name",
+        validate: (nameInput) => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log("Please enter the correct engineer's name!");
+            return false;
+          }
+        },
+      },
+
+      {
+        type: "number",
+        name: "id",
+        message: "Please enter engineer's id",
+        validate: (idInput) => {
+          if (idInput) {
+            return true;
+          } else {
+            console.log(
+              "Please enter the correct engineer's id, this should be a number!"
+            );
+            return false;
+          }
+        },
+      },
+
+      //Email address
+      {
+        type: "input",
+        name: "email",
+        message: "Please enter engineer's email",
+        validate: (emailInput) => {
+          if (emailInput) {
+            return true;
+          } else {
+            console.log("Please enter the correct engineer's email!");
+            return false;
+          }
+        },
+      },
+
+      //Office number
+      {
+        type: "number",
+        name: "officeNumber",
+        message: "Please enter engineer's office Number",
+        validate: (idInput) => {
+          if (idInput) {
+            return true;
+          } else {
+            console.log(
+              "Please enter the correct engineer's office Number, this should be a number!"
+            );
+            return false;
+          }
+        },
+      },
+    ])
+    //pushes engineer;s data into teamInfo array
+    .then((engineerData) => {
+      // Create a new Manager object and push it to teamInfo Array
+      const newEngineer = new Engineer(
+        engineerData.name,
+        engineerData.id,
+        engineerData.email,
+        engineerData.officeNumber
+      );
+      this.teamInfo.push(newEngineer);
+      // Send user back to the menu for more input
+      this.promptUserType();
+    });
+}
+function internPrompt() {
+  inquirer
+    .prompt([
+      //Name
+      {
+        type: "input",
+        name: "name",
+        message: "Please enter Intern's name",
+        validate: (nameInput) => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log("Please enter the correct Intern's name!");
+            return false;
+          }
+        },
+      },
+
+      {
+        type: "number",
+        name: "id",
+        message: "Please enter Intern's id",
+        validate: (idInput) => {
+          if (idInput) {
+            return true;
+          } else {
+            console.log(
+              "Please enter the correct Intern's id, this should be a number!"
+            );
+            return false;
+          }
+        },
+      },
+
+      //Email address
+      {
+        type: "input",
+        name: "email",
+        message: "Please enter Intern's email",
+        validate: (emailInput) => {
+          if (emailInput) {
+            return true;
+          } else {
+            console.log("Please enter the correct Intern's email!");
+            return false;
+          }
+        },
+      },
+
+      //Office number
+      {
+        type: "number",
+        name: "officeNumber",
+        message: "Please enter Intern's office Number",
+        validate: (idInput) => {
+          if (idInput) {
+            return true;
+          } else {
+            console.log(
+              "Please enter the correct Intern's office Number, this should be a number!"
+            );
+            return false;
+          }
+        },
+      },
+    ])
+    //pushes intern's data into teamInfo array
+    .then((internData) => {
+      // Create a new Manager object and push it to teamInfo Array
+      const newIntern = new Intern(
+        internData.name,
+        internData.id,
+        internData.email,
+        internData.officeNumber
+      );
+      this.teamInfo.push(newIntern);
+      // Send user back to the menu for more input
+      this.promptUserType();
+    });
+}
+// Generate HTML page based on teamArray and write it to a file
+function generateHTML() {
+  const htmlPage = render(this.teamInfo());
+  fs.writeFile(outputPath, htmlPage, (err) =>
+    err
+      ? console.log(err)
+      : console.log("HTML file has been created in the lib/folder")
+  );
+}
+//start prompt for manager
+
+managerPrompt();
